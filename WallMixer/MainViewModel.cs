792e6bf307cae47f21d0027d899a086955310103
Justin Mangum <parabola949@gmail.com>
2017-11-23 15:03:53 -0600

    Can now save wallpapers from multiple monitors. Will add ability to select which wallpaper later

--- WallMixer/MainViewModel.cs
+++ WallMixer/MainViewModel.cs
@@ -22,12 +22,14 @@
         private IWallpaperRepository _db;
         private IMetroDialog _dialog;
         private Random _rand;
-        private string currentImage;
+        private List<string> currentImages = new List<string>();
+        private List<Image> newImages = new List<Image>();
+
         private CancellationTokenSource _cts;
 
         public string ToolTipInfo { get; set; }
         public bool OptionEnabled { get; set; }
-        
+
         [ImportingConstructor]
         public MainViewModel(IWindowManager manager, IWallpaperRepository db, IMetroDialog dialog)
         {
@@ -48,6 +50,9 @@
                     var sources = await _db.GetSourcesAsync();
                     if (sources.Count > 0)
                     {
+                        foreach (var file in currentImages)
+                            File.Delete(file);
+                        
                         OptionEnabled = true;
                         NotifyOfPropertyChange(() => OptionEnabled);
                         var randSource = sources[_rand.Next(sources.Count)];
@@ -55,44 +60,51 @@
                         Image image = null;
                         var isMultiple = await _db.UseMultiple();
                         string tempFile;
-                        if (isMultiple) {
+                        if (isMultiple)
+                        {
                             //testing for multiple monitors
                             var screens = System.Windows.Forms.Screen.AllScreens;
                             //so, we use the bounds.location.x / y to calculate the screen positions
                             var test = screens.First().WorkingArea;
                             //this needs to be modified to allow vertically stacked monitors.
                             //right now only side by side monitors will work
-                            var bmp = new Bitmap(screens.Sum(x => x.Bounds.Width), screens.Max(x => x.Bounds.Height));
-                            var g = Graphics.FromImage(bmp);
-                            
-                            //get a random image for each screen
-                            foreach (var s in screens)
+                            using (var bmp = new Bitmap(screens.Sum(x => x.Bounds.Width), screens.Max(x => x.Bounds.Height)))
+                            using (var g = Graphics.FromImage(bmp))
                             {
-                                string imageUrl = await UrlFetcher.GetRandomImageUrl(randSource, await _db.ImgurClientId());
-                                tempFile = await WebImage.DownloadImage(imageUrl);
-                                image = Image.FromFile(tempFile);
-                                //draw it onto the screens bounds
-                                g.DrawImage(image, new Rectangle(s.Bounds.Location.X, s.Bounds.Location.Y, s.Bounds.Width, s.Bounds.Height));
+
+                                //get a random image for each screen
+                                foreach (var s in screens)
+                                {
+                                    string imageUrl = await UrlFetcher.GetRandomImageUrl(randSource, await _db.ImgurClientId());
+                                    tempFile = await WebImage.DownloadImage(imageUrl);
+                                    currentImages.Add(tempFile);
+                                    image = Image.FromFile(tempFile);
+                                    //draw it onto the screens bounds
+                                    g.DrawImage(image, new Rectangle(s.Bounds.Location.X, s.Bounds.Location.Y, s.Bounds.Width, s.Bounds.Height));
+                                    newImages.Add(image);
+                                }
+                                //now get the file out of it
+                                g.Save();
+                                tempFile = Path.Combine(Environment.ExpandEnvironmentVariables("%temp%"), "stitched.bmp");
+                                bmp.Save(tempFile);
                             }
-                            //now get the file out of it
-                            g.Save();
-                            tempFile = Path.Combine(Environment.ExpandEnvironmentVariables("%temp%"), "stitched.bmp");
-                            bmp.Save(tempFile);
                         }
                         else
                         {
                             string imageUrl = await UrlFetcher.GetRandomImageUrl(randSource, await _db.ImgurClientId());
                             tempFile = await WebImage.DownloadImage(imageUrl);
                             image = Image.FromFile(tempFile);
+                            newImages.Add(image);
+                            currentImages.Add(tempFile);
                         }
 
                         DesktopBackground.SetWallpaper(tempFile, isMultiple);
                         ToolTipInfo = string.Format("Source: {0}\r\nResolution: {1}",
-                            randSource.Source == Source.Reddit ? randSource.Query : randSource.Source + " / " + randSource.Query, image.Width + "x" + image.Height);
+                            randSource.Source == Source.Reddit ? randSource.Query : randSource.Source + " / " + randSource.Query, newImages.Aggregate("", (c, v) => $"{c}\r\n{v.Width}x{v.Height}"));
                         NotifyOfPropertyChange(() => ToolTipInfo);
-                        image.Dispose();
-                        if (!string.IsNullOrEmpty(currentImage)) File.Delete(currentImage);
-                        currentImage = tempFile;
+                        foreach (var i in newImages)
+                            i.Dispose();
+                        newImages.Clear();
                     }
                     else
                     {
@@ -111,7 +123,7 @@
                     NotifyOfPropertyChange(() => OptionEnabled);
                     await Task.Delay(TimeSpan.FromSeconds(30));
                 }
-                
+
             }
         }
 
@@ -122,7 +134,8 @@
 
         public async void SaveWallpaper()
         {
-            File.Copy(currentImage, (await _db.SaveLocation()) + @"\" + Path.GetFileName(currentImage));
+            foreach (var i in currentImages)
+                File.Copy(i, (await _db.SaveLocation()) + @"\" + Path.GetFileName(i));
         }
 
         public void NextWallpaper()
